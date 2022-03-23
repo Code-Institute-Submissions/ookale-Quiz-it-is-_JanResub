074fcc8e13e4991670af46f0d45c8337dffec8ff
ookale <ookale@gmail.com>
2022-03-23 17:49:28 +0000

    Add code to js for startGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-const startButton = document.getElementById("start-btn");
-const quizForm = document.getElementById("quiz-form");
+const startButton = document.getElementById("start-btn")
+const quizForm = document.getElementById("quiz-form")
 
 startButton.addEventListener("click", startGame)
 
 function startGame() {
     startButton.classList.add("hide")
-    quizForm.classList.remove("hide");
+    quizForm.classList.add("remove")
+    getNextQuestion()
 }
 
-const scoreBoard = document.getElementsByClassName(".scoreboard");
-const options = document.getElementsByClassName("all-options");
-const option = document.getElementsByClassName("option");
-const question = document.getElementsByClassName("quest");
-const cancelButton = document.getElementById("cancel");
-const nextButton = document.getElementById("next");
+const scoreBoard = document.getElementsByClassName("scoreboard")
+const options = document.getElementsByClassName("all-options")
+const option = document.getElementsByClassName("option")
+const question = document.getElementsByClassName("quest")
+const cancelButton = document.getElementById("cancel")
+const nextButton = document.getElementById("next")
+const containerElements = document.getElementsByClassName("container")
 
 
