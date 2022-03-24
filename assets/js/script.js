77094a19cb035bb3a5b93c010bfbb4435ae9504e
ookale <ookale@gmail.com>
2022-03-24 11:24:43 +0000

    Style first page and code start button for quiz now working, add questions to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
 const startButton = document.getElementById("start-btn")
 const quizForm = document.getElementById("quiz-form")
+const scoreBoard = document.getElementById("scoreboard")
 
 startButton.addEventListener("click", startGame)
 
 function startGame() {
     startButton.classList.add("hide")
-    quizForm.classList.add("remove")
+    quizForm.classList.remove("hide")
+    scoreBoard.classList.remove("hide")
     getNextQuestion()
 }
 
-const scoreBoard = document.getElementsByClassName("scoreboard")
-const options = document.getElementsByClassName("all-options")
-const option = document.getElementsByClassName("option")
-const question = document.getElementsByClassName("quest")
+
 const cancelButton = document.getElementById("cancel")
 const nextButton = document.getElementById("next")
 const containerElements = document.getElementsByClassName("container")
+
+const questions = [
+    {
+      question: "Which player was sent off in his final game during the final of the 2006 Football World Cup?",
+      answers: [
+          { text: "Zinedine Zidane", correct: true },
+          { text: "Marco Materazzi", correct: false},   
+          { text: "Michael Ballack", correct: false},
+          { text: "Luis Figo", correct: false}
+      ]
+    },
+    {
+        question: "What was Wayne Gretzky's nickname?",
+        answers:[
+            { text: "The Next One", correct: false },
+            { text: "The Best One", correct: false },
+            { text: "The Great One", correct: true },
+            { text: "The First One", correct: false },
+        ]
+    }
+]
 
 
