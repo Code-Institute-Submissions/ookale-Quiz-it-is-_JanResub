0bf4631e0296fbcfc1d33e1a740091c8e9e6f572
ookale <ookale@gmail.com>
2022-04-22 13:36:10 +0000

    Add next questiion, show question function and start work on show results

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,29 @@ const containerElements = document.getElementsByClassName("container");
 const questionSpace = document.getElementById("question");
 const answerOptions = document.getElementsByClassName("answer-box");
 let shuffledQuestions, currentQuestion
+let correctAnswers
+let score = 0
+let radioButtonLabel1 = document.getElementById("optionLabel1");
+let radioButton1 = document.getElementById("option1");
+let radioButtonLabel2 = document.getElementById("optionLabel2");
+let radioButton2 = document.getElementById("option2");
+let radioButtonLabel3 = document.getElementById("optionLabel3");
+let radioButton3 = document.getElementById("option3");
+let radioButtonLabel4 = document.getElementById("optionLabel4");
+let radioButton4 = document.getElementById("option4");
+
+let radioButtons = [
+  radioButton1,
+  radioButton2,
+  radioButton3,
+  radioButton4
+];
+let radioButtonLabels = [
+  radioButtonLabel1,
+  radioButtonLabel2,
+  radioButtonLabel3,
+  radioButtonLabel4
+];
 
 startButton.addEventListener("click", startGame)
 
@@ -22,15 +45,59 @@ function startGame() {
 nextButton.addEventListener("click", nextQuestion)
 
 function nextQuestion() {
+  // hitta knappen som är vald
+  const selectedButton = radioButtons.forEach(radioButton => {
+    {radioButton.checked === true}
+  });
+  console.log({selectedButton})
+
+  // hitta vilken label som hör till den valda knappen
+  const selectedLabel = radioButtonLabels.forEach(label => {
+    {label.htmlFor === selectedButton?.id;console.log(label.htmlFor, selectedButton)}
+  });
+  console.log(selectedLabel?.innerText);
+  
+  // radioButtons.forEach(
+  //   radioButtonLabel => document.getElementById(radioButtonLabel.htmlFor)?.checked === true
+  // )
+  
+  console.log(selectedLabel)
+  
+    // spara undan nuvarande fråga i variabel
+    const myQuestion = shuffledQuestions[currentQuestion]
+    // spara undan alla alternativ som tillhör nuvarande fråga i en variabel
+    const myAnswers = myQuestion.answers;
+    // Gå igenom all alternativ och hitta det som stämmer med label texten för det valda svaret
+    const myAnswer = myAnswers.forEach(
+      answer => answer.text === selectedLabel?.innerText
+      )
+    console.log(myAnswer)
+    //Kontrollera om svaret är rätt
+  // checkAnswer(mittsvar)
+  if (shuffledQuestions.length === currentQuestion +1)
+    {nextButton.innerText = "Show Result" 
+  nextButton.addEventListener("click", showResult)}
+    else { 
     showQuestion (shuffledQuestions[currentQuestion])
-   }
+    currentQuestion = currentQuestion +1
+   }}
 
    function showQuestion(question) {
        questionSpace.innerText = question.question
-       question.answers.forEach(answer => {
-         const label = document.createElement("label");
-         label.innerText = answer.text
-       })
+       for (let i = 0; i < question.answers.length; i++) {
+        const label = document.getElementById("optionLabel" + `${i+1}`);
+        label.innerText = question.answers[i].text
+      }
+   }
+
+   function showResult() {
+     correctAnswers = document.getElementById("correctAnswers")
+     correctAnswers.innerText = "You scored" + `${score}`
+   }
+
+   function checkAnswer(answer) {
+     if (answer.correct === true)
+     score = score +1
    }
 
    
